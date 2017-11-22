e7f266d7db457083a6696af472314ad8e73a30db
Romain Clavel <romain@clavel.io>
2017-11-22 17:06:48 +0100

    Implement tic tac plouf with time travel

---
+++ src/index.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+function HistoryLine(props) {
+  return (
+    <li>
+      <button onClick={() => props.onClick(props.i)}>
+        {`Rollback ${props.i} moves`}
+      </button>
+    </li>
+  );
+}
+
+function History(props) {
+  const historyLines = props.history.slice(1).map((step, index) => {
+    return <HistoryLine onClick={props.onClick} i={index + 1} key={index} />
+  })
+
+  return <ol>{historyLines}</ol>;
+}
+
+class TheGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.history[0].squares.slice();
+
+    if (findWinner(squares) || squares[i]) return;
+    squares[i] = this.state.xIsNext ? 'X' : 'M';
+    this.setState({
+      history: [{ squares: squares }].concat(this.state.history),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  timeTravel(i) {
+    this.setState({
+      history: this.state.history.slice(i),
+      xIsNext: i % 2 ? this.state.xIsNext : !this.state.xIsNext,
+    });
+  }
+
+  render() {
+    const winner = findWinner(this.state.history[0].squares);
+    const status = winner ? `WHO WON?! >>${winner}<<` : `Next noob: ${this.state.xIsNext ? 'X' : 'M'}`;
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={this.state.history[0].squares}
+            xIsNext={this.state.xIsNext}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div className="status">{status}</div>
+          <History history={this.state.history} onClick={(i) => this.timeTravel(i)} />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <TheGame />,
+  document.getElementById('root')
+);
+
+function findWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
